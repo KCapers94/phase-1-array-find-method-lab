dfcbcaf42ba50adadda903df86f8a75ff40fc1a1
Keith Capers <K.Capers94@gmail.com>
2023-11-07 21:49:24 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(record){
+    let foundSuperBowl = record.find((element) =>{
+        if(element.result === "W"){
+            return true
+        }
+    })
+    if(foundSuperBowl === undefined){
+        return foundSuperBowl
+    }
+    return foundSuperBowl.year
+  }
+  
